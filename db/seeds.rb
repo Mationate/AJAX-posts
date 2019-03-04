91e293903acea50532200098f1c8676b07b395db
Matias Oñate <mationate.0@gmail.com>
2019-03-04 14:28:45 -0300

    ADD seed with Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,26 @@
+User.destroy_all
+Company.destroy_all
+30.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: 123456
+  )
+end
+users = User.all
+20.times do
+  Company.create(
+    name: Faker::GreekPhilosophers.name
+  )
+end
 
+companies = Company.all
+250.times do
+  Complain.create(
+    content: Faker::GreekPhilosophers.quote,
+    user: users.sample,
+    company: companies.sample
+  )
+end
 
 if Rails.env.development?
   AdminUser.destroy_all
