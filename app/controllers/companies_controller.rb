9c4735f246ae875729e83800232cd4c4b1f5e9d6
Matias Oñate <mationate.0@gmail.com>
2019-03-04 11:04:41 -0300

    create companies via AJAX request

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,23 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
+    @company = Company.new
   end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      respond_to :js
+    else
+      redirect_to root_path, alert:"Error al crear una empresa, intente nuevamente"
+      
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name)
+  end
+  
 end
